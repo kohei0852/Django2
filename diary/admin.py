b4e988a1e0d2d985ccfdd790bc2236d57689bbb7
narumi kouhei <g206068@hi-tech.ac.jp>
2023-11-10 01:21:32 +0000

    11月11章のコミット作成

---
+++ diary/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from .models import Diary
+
+
+admin.site.register(Diary)
+
+# Register your models here.
---
+++ private_diary/settings_dev.py
@@ -0,0 +1,52 @@
+from .settings_common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-mxiy*$y!l$fvxvw%isx53l&mj-7vx*w=7d6nhbf^q)(&lfr7+a'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# ロギング設定
+LOGGING = {
+    'version': 1, # 1固定
+    'disable_existing_loggers': False,
+
+    # ロガーの設定
+    'loggers': {
+        # Djangoの利用するロガー
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        # diaryアプリケーションが利用するロガー
+        'diary': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+
+    # ハンドラーの設定
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev',
+        },
+    },
+
+    # フォーマッターの設定
+    'formatters': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    },
+}
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
